53c62ddcb7af8c95a3331c78690869e81ef13272
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-04-05 20:51:24 -0300

    Clean up

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
-
-const getBuildConfig = () => ({});
-
-module.exports = (phase) => {
-  const shouldAddBuildConfig =
-    phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD;
-  return shouldAddBuildConfig ? getBuildConfig() : {};
-};
