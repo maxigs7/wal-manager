f67d1fb9d4b1f1ec242fbb635e7bbb06b17f593f
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-04-13 16:37:09 -0300

    Add new packages

---
+++ backend/src/constants.ts
@@ -0,0 +1,6 @@
+export const INFRASTRUCTURE_TOKEN_FACTORY = 'TokenFactory';
+export const INFRASTRUCTURE_USER_MANAGER = 'UserManager';
+
+export const PERSISTENCE_ACCOUNT_REPOSITORY = 'AccountRepository';
+export const PERSISTENCE_CATEGORY_REPOSITORY = 'CategoryRepository';
+export const PERSISTENCE_USER_REPOSITORY = 'UserRepository';
