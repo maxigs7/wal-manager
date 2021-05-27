29f9ef819eb2e846e06830eddd8998095848e1e5
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-05-27 09:57:20 -0300

    Add webpack analyze

--- src/routes/default.route.tsx
+++ src/routes/default.route.tsx
@@ -3,7 +3,9 @@ import { RouteProps } from 'react-router-dom';
 
 export const defaultRoutes: RouteProps[] = [
   {
-    component: lazy(() => import('@app/pages/auth/login')),
+    component: lazy(
+      () => import(/* webpackChunkName: 'auth.login.page' */ '@app/pages/auth/login'),
+    ),
     path: '/auth/login',
   },
 ];
