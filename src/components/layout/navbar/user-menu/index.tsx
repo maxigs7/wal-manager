673640b7df3928ab2eb84e89cccff9a9af1c0fe4
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-05-27 09:02:49 -0300

    Add dropdown on user button. Add sign out

--- src/components/layout/navbar/user-menu/index.tsx
+++ src/components/layout/navbar/user-menu/index.tsx
@@ -1,62 +1,88 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/solid';
+import classnames from 'classnames';
 
 import { useAuth } from '@lib/auth';
 
 export const UserMenu: React.FC = React.memo(() => {
-  const { user } = useAuth();
-  // const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  // const trigger = useRef(null);
-  // const dropdown = useRef(null);
-
-  // close on click outside
-  // useEffect(() => {
-  //   const clickHandler = ({ target }) => {
-  //     if (!dropdownOpen || dropdown.current.contains(target) || trigger.current.contains(target))
-  //       return;
-  //     setDropdownOpen(false);
-  //   };
-  //   document.addEventListener('click', clickHandler);
-  //   return () => document.removeEventListener('click', clickHandler);
-  // });
-
-  // close if the esc key is pressed
-  // useEffect(() => {
-  //   const keyHandler = ({ keyCode }) => {
-  //     if (!dropdownOpen || keyCode !== 27) return;
-  //     setDropdownOpen(false);
-  //   };
-  //   document.addEventListener('keydown', keyHandler);
-  //   return () => document.removeEventListener('keydown', keyHandler);
-  // });
+  const { user, signOut } = useAuth();
 
   return (
-    <div className="h-full">
-      <button
-        // ref={trigger}
-        aria-haspopup="true"
-        className="flex justify-center items-center p-3 h-full focus:outline-none"
-        // onClick={() => setDropdownOpen(!dropdownOpen)}
-        // aria-expanded={dropdownOpen}
-      >
-        {user?.photoURL && (
-          <img
-            alt="User"
-            className="w-8 h-8 rounded-full"
-            height="32"
-            src={user?.photoURL}
-            width="32"
-          />
-        )}
-        <div className="flex items-center truncate">
-          <span className="truncate ml-2 text-sm font-medium hidden sm:inline">
-            {user?.displayName}
-          </span>
-          <ChevronDownIcon className="w-5 h-5 flex-shrink-0 ml-1 fill-current text-gray-400" />
-        </div>
-      </button>
-    </div>
+    <Menu as="div" className="relative">
+      {({ open }) => (
+        <>
+          <div>
+            <Menu.Button className="inline-flex justify-center items-center p-3 focus:outline-none">
+              {user?.photoURL && (
+                <img
+                  alt="User"
+                  className="w-8 h-8 rounded-full"
+                  height="32"
+                  src={user?.photoURL}
+                  width="32"
+                />
+              )}
+              <span className="truncate ml-2 text-sm font-medium hidden sm:inline">
+                {user?.displayName}
+              </span>
+              <ChevronDownIcon
+                aria-hidden="true"
+                className="w-5 h-5 flex-shrink-0 ml-1 fill-current text-gray-400"
+              />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+            show={open}
+          >
+            <Menu.Items
+              className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+              static
+            >
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      className={classnames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      )}
+                      href="#"
+                    >
+                      Profile
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      className={classnames(
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      )}
+                      href="#"
+                      onClick={() => signOut()}
+                    >
+                      Sign Out
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
   );
 });
