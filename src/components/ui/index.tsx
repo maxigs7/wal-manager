ea5dbfa7e31524c2db0583d678bfecbd711cb52c
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-06-07 15:57:59 -0300

    Fix component ui imports

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,3 +1,4 @@
 export { default as Avatar } from './avatar';
 export { default as Button } from './buttons';
 export { default as CardContainer } from './card-container';
+export { default as ImageWithFallback } from './image-with-fallback';
