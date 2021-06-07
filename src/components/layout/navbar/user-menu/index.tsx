18618c6e59ad8002252b7bbd8e3eced7c8a8baa3
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-06-07 14:19:03 -0300

    Update avatar component

--- src/components/layout/navbar/user-menu/index.tsx
+++ src/components/layout/navbar/user-menu/index.tsx
@@ -1,11 +1,21 @@
 import React, { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Menu, Transition } from '@headlessui/react';
 
 import { Avatar } from '@app/components/ui';
 import { useAuth } from '@lib/auth';
-import classnames from 'classnames';
+import classnames from '@lib/classnames';
+
+const styles = {
+  displayName: 'truncate ml-2 text-sm font-medium hidden sm:inline',
+  button: 'inline-flex justify-center items-center p-3 focus:outline-none',
+  buttonArrow: 'w-5 h-5 flex-shrink-0 ml-1 fill-current text-gray-400',
+  item: 'origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none',
+  itemLink: (isActive: boolean) =>
+    classnames(isActive ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm'),
+};
 
 export const UserMenu: React.FC = React.memo(() => {
   const { user, signOut } = useAuth();
@@ -15,15 +25,10 @@ export const UserMenu: React.FC = React.memo(() => {
       {({ open }) => (
         <>
           <div>
-            <Menu.Button className="inline-flex justify-center items-center p-3 focus:outline-none">
-              {user?.photoURL && <Avatar alt="User Profile" size="sm" src={user?.photoURL} />}
-              <span className="truncate ml-2 text-sm font-medium hidden sm:inline">
-                {user?.displayName}
-              </span>
-              <FontAwesomeIcon
-                className="w-5 h-5 flex-shrink-0 ml-1 fill-current text-gray-400"
-                icon="chevron-down"
-              />
+            <Menu.Button className={styles.button}>
+              {user?.photoURL && <Avatar alt="User Profile" src={user?.photoURL} />}
+              <span className={styles.displayName}>{user?.displayName}</span>
+              <FontAwesomeIcon className={styles.buttonArrow} icon="chevron-down" />
             </Menu.Button>
           </div>
 
@@ -37,36 +42,20 @@ export const UserMenu: React.FC = React.memo(() => {
             leaveTo="transform opacity-0 scale-95"
             show={open}
           >
-            <Menu.Items
-              className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
-              static
-            >
+            <Menu.Items className={styles.item} static>
               <div className="py-1">
                 <Menu.Item>
                   {({ active }) => (
-                    <a
-                      className={classnames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm',
-                      )}
-                      href="#"
-                    >
+                    <NavLink className={styles.itemLink(active)} to="/settings" exact>
                       Profile
-                    </a>
+                    </NavLink>
                   )}
                 </Menu.Item>
               </div>
               <div className="py-1">
                 <Menu.Item>
                   {({ active }) => (
-                    <a
-                      className={classnames(
-                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                        'block px-4 py-2 text-sm',
-                      )}
-                      href="#"
-                      onClick={() => signOut()}
-                    >
+                    <a className={styles.itemLink(active)} href="#" onClick={() => signOut()}>
                       Sign Out
                     </a>
                   )}
--- src/components/ui/avatar/index.tsx
+++ src/components/ui/avatar/index.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
-import classnames from 'classnames';
+import classnames from '@lib/classnames';
 
-type AvatarSize = 'lg' | 'md' | 'sm';
-
-export interface AvatarProps extends React.ComponentPropsWithoutRef<'img'> {
-  size: AvatarSize;
+export enum AvatarSizes {
+  SM = 'sm',
+  MD = 'md',
+  LG = 'lg',
 }
 
-const Avatar: React.FC<AvatarProps> = ({ size = 'md', className, ...htmlAttributes }) => {
-  return (
-    <img
-      className={classnames(
-        'inline object-cover rounded-full',
-        size !== 'sm' && 'mr-2',
-        size === 'sm' && 'w-8 h-8',
-        size === 'md' && 'w-12 h-12',
-        size === 'lg' && 'w-16 h-16',
-        className,
-      )}
-      {...htmlAttributes}
-    />
-  );
+const SizesMap: { [key in AvatarSizes | string]: string } = {
+  [AvatarSizes.SM]: 'w-8 h-8',
+  [AvatarSizes.MD]: 'mr-2w-12 h-12',
+  [AvatarSizes.LG]: 'mr-2 w-16 h-16',
+};
+
+const styles = {
+  image: (size: string) => classnames('inline object-cover rounded-full', SizesMap[size]),
+};
+
+export interface AvatarProps extends React.ComponentPropsWithoutRef<'img'> {
+  size?: AvatarSizes;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ size = AvatarSizes.MD, className, ...htmlAttributes }) => {
+  return <img className={classnames(styles.image(size), className)} {...htmlAttributes} />;
 };
 
 export default Avatar;
