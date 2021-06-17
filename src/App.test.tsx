c44455f6f7c159b0b035d8ad732d9e1931bc7cd6
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-06-17 09:02:38 -0300

    Fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(container).toBeInTheDocument();
 });
