19ef4ac2cd6d30f7da1f2f3b63309cea537581c7
Maximiliano Gonzalez Schweizer <mschweizer@sparkdigital.com>
2021-06-18 09:09:16 -0300

    Add named export for classnames function

--- src/lib/classnames/index.tsx
+++ src/lib/classnames/index.tsx
@@ -1,2 +1,6 @@
-export default (...classes: (false | null | undefined | string)[]): string =>
+const classnames = (...classes: (false | null | undefined | string)[]): string =>
   classes.filter(Boolean).join(' ');
+
+export default classnames;
+
+export { classnames };
