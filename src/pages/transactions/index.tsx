8a21fe8ecdfee513c0d7c23e79d898975e754115
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2021-11-24 08:41:16 -0300

    Transaction page

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,55 +1,33 @@
-import React, { useMemo, useState } from 'react';
+import React from 'react';
 
 import { Button } from '@chakra-ui/react';
-import { addMilliseconds, addMonths } from 'date-fns';
 
-import { useTransactionList } from '@api';
-import { MonthsTab, TransactionTable, YearBar } from '@components';
-import { TransactionPortalModal } from '@containers';
-import { Card, Page } from '@lib/wal-ui';
+import { TransactionPortalModal, TransactionsList } from '@containers';
+import { Page } from '@lib/wal-ui';
 import { TransactionType } from '@models';
+import { useTransactionsStore } from '@stores';
 
 const TransactionsPage: React.FC = () => {
-  const [year, setYear] = useState(2021);
-  const [month, setMonth] = useState(10);
-  const [type, setType] = useState<TransactionType>();
-  const [isOpenForm, setIsOpenForm] = useState(false);
-
-  const startDate = useMemo(() => new Date(year, month, 1), [year, month]);
-  const endDate = useMemo(() => addMilliseconds(addMonths(startDate, 1), -1), [startDate]);
-  const { data } = useTransactionList(startDate, endDate);
+  const [state, dispatch] = useTransactionsStore();
 
   return (
     <Page metaTitle="Movimientos" title="Movimientos">
-      <Button
-        onClick={() => {
-          setType(TransactionType.Income);
-          setIsOpenForm(true);
-        }}
-      >
-        Nuevo Ingreso
-      </Button>
-      <Button
-        onClick={() => {
-          setType(TransactionType.Expense);
-          setIsOpenForm(true);
-        }}
-      >
-        Nuevo Gasto
-      </Button>
+      <Button onClick={() => dispatch.onOpenForm(TransactionType.Income)}>Nuevo Ingreso</Button>
+      <Button onClick={() => dispatch.onOpenForm(TransactionType.Expense)}>Nuevo Gasto</Button>
       <TransactionPortalModal
-        isOpenForm={isOpenForm}
-        isOpenRemove={false}
-        onConfirmed={() => setIsOpenForm(false)}
-        onDismiss={() => setIsOpenForm(false)}
-        type={type}
+        isOpenForm={state.isOpenForm}
+        isOpenRemove={state.isOpenRemove}
+        onConfirmed={dispatch.onConfirmedForm}
+        onDismiss={dispatch.onDismissForm}
+        type={state.selectedType}
       />
 
-      <Card>
-        <YearBar currentYear={year} onUpdateYear={setYear} />
-        <MonthsTab currentMonth={month} onTabChange={setMonth} />
-        <TransactionTable data={data || []} />
-      </Card>
+      <TransactionsList
+        month={state.month}
+        setMonth={dispatch.onChangedMonth}
+        setYear={dispatch.onChangedYear}
+        year={state.year}
+      />
     </Page>
   );
 };
