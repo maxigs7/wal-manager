92432989aa5119cb3eba48ad492b72d7e8e9e6f6
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2022-09-12 18:54:18 -0300

    Fix build

--- src/modules/transaction/containers/credit-card-summary/index.tsx
+++ src/modules/transaction/containers/credit-card-summary/index.tsx
@@ -1,35 +1,36 @@
-import { CreditCardStat } from '@m/credit-card';
-import React from 'react';
+// import { CreditCardStat } from '@m/creditCard';
+// import React from 'react';
 
-import { ContentLoader } from '@shared';
+// import { ContentLoader } from '@shared';
 
-import { useCreditCardSummary } from '../../hooks';
-import { useTransactionStore } from '../../providers';
+// import { useCreditCardSummary } from '../../hooks';
+// import { useTransactionStore } from '../../providers';
 
-const Summary: React.FC = () => {
-  const [{ account, startDate, endDate }] = useTransactionStore();
+// const Summary: React.FC = () => {
+//   const [{ account, startDate, endDate }] = useTransactionStore();
 
-  const { data: creditCards, isLoading: isLoadingCreditCards } = useCreditCardSummary(
-    account?.id,
-    startDate,
-    endDate,
-  );
+//   const { data: creditCards, isLoading: isLoadingCreditCards } = useCreditCardSummary(
+//     account?.id,
+//     startDate,
+//     endDate,
+//   );
 
-  if (!account) {
-    return null;
-  }
-  if (isLoadingCreditCards) return <ContentLoader />;
-  if (!creditCards || !creditCards.length) {
-    return null;
-  }
+//   if (!account) {
+//     return null;
+//   }
+//   if (isLoadingCreditCards) return <ContentLoader />;
+//   if (!creditCards || !creditCards.length) {
+//     return null;
+//   }
 
-  return (
-    <>
-      {creditCards.map((cc) => (
-        <CreditCardStat key={cc.cc} amount={cc.amount} label={cc.cc} type={cc.type} w="full" />
-      ))}
-    </>
-  );
-};
+//   return (
+//     <>
+//       {creditCards.map((cc) => (
+//         <CreditCardStat key={cc.cc} amount={cc.amount} label={cc.cc} type={cc.type} w="full" />
+//       ))}
+//     </>
+//   );
+// };
 
-export default React.memo(Summary);
+// export default React.memo(Summary);
+export {};
