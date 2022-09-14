6c936bdcc0458309de29f39c74542137fd6fd426
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2022-09-14 12:06:53 -0300

    Fix ddl on transaction form

--- src/modules/creditCard/components/select-control/index.tsx
+++ src/modules/creditCard/components/select-control/index.tsx
@@ -4,7 +4,7 @@ import { Control, RegisterOptions, useController } from 'react-hook-form';
 import { CreditCardSelect, ICreditCardSelectProps } from '../select';
 
 export interface ICreditCardSelectControlProps
-  extends Omit<ICreditCardSelectProps, 'onBlur' | 'onChange' | ''> {
+  extends Omit<ICreditCardSelectProps, 'onBlur' | 'onChange'> {
   control?: Control<any>;
   rules?: RegisterOptions;
 }
@@ -17,6 +17,7 @@ const CreditCardSelectControl: React.FC<ICreditCardSelectControlProps> = ({
   name,
   placeholder,
   rules,
+  ...props
 }) => {
   const { field } = useController({
     name,
@@ -26,6 +27,7 @@ const CreditCardSelectControl: React.FC<ICreditCardSelectControlProps> = ({
 
   return (
     <CreditCardSelect
+      {...props}
       {...field}
       creditCards={creditCards}
       id={id}
