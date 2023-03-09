2f550a69cf5e0083ddbcd74cf59b0b5d64c47c19
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2023-03-09 08:29:49 -0300

    Add tailwindcss and configurations

---
+++ theme/breakpoints.js
@@ -0,0 +1,9 @@
+const breakpoints = {
+  sm: '425px',
+  md: '720px',
+  lg: '960px',
+  xl: '1140px',
+  '2xl': '1320px',
+};
+
+module.exports = breakpoints;
---
+++ theme/typography.js
@@ -0,0 +1,7 @@
+const typography = {
+  body: ['var(--font-roboto)', 'sans-serif'],
+  sans: ['var(--font-roboto)', 'sans-serif'],
+  serif: ['var(--font-roboto-slab)', 'serif'],
+};
+
+module.exports = typography;
